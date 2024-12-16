140e020e4cd0ff17572025de1c69ce9e3ab6d89a
Raeonn <lyraeon5@gmail.com>
2024-12-16 22:00:00 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let textContent = document.body.innerText; // Get text without HTML tags 
-console.log(textContent);
+function order(num,server,zone,item){
+    const n = document.getElementById(num);
+    const s = document.getElementById(server);
+    const z = document.getElementById(zone);
+    const i = document.getElementById(item);
+    
+    window.location.href = "https://wa.me/601151722487?text="+n.value;
+}
